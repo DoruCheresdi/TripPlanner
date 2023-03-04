41a040c6e01eb94b53fb17de38dcf49a8fb5720a
alexandrubuzea <alexandru.buzea2007@stud.acs.upb.ro>
2023-03-04 22:52:14 +0200

    java stuff

--- src/main/java/com/tripplanner/tripplanner/controller/RouteController.java
+++ src/main/java/com/tripplanner/tripplanner/controller/RouteController.java
@@ -1,5 +1,6 @@
 package com.tripplanner.tripplanner.controller;
 
+import com.tripplanner.tripplanner.entities.route.Route;
 import com.tripplanner.tripplanner.service.routeSearch.GetRouteService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,7 @@ public class RouteController {
   }
 
   @GetMapping(value = "/devapi/findroute")
-  public String getRoute(@RequestParam String source, @RequestParam String destination) {
+  public Route getRoute(@RequestParam String source, @RequestParam String destination) {
 
     return routeService.doRouteService(destination, source);
   }
--- src/main/java/com/tripplanner/tripplanner/service/routeSearch/GetRouteService.java
+++ src/main/java/com/tripplanner/tripplanner/service/routeSearch/GetRouteService.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.tripplanner.tripplanner.entities.place.Place;
 import com.tripplanner.tripplanner.entities.place.Position;
+import com.tripplanner.tripplanner.entities.route.Route;
 import com.tripplanner.tripplanner.secret.GoogleKey;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.RouteMatcher;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -16,7 +18,7 @@ import java.util.List;
 @Service
 @Slf4j
 public class GetRouteService {
-  public String doRouteService(String destination, String location) {
+  public Route doRouteService(String destination, String location) {
     log.info("Doing routing search from {} to {}", location, destination);
 
     RestTemplate restTemplate = new RestTemplate();
@@ -43,13 +45,36 @@ public class GetRouteService {
       }
 
       JsonNode routes = root.path("routes");
-      JsonNode info = ((ArrayNode) routes).get(0);
+      JsonNode info = ((ArrayNode) routes).get(0).path("legs").get(0);
+
+      JsonNode start_location = info.path("start_location");
+      JsonNode end_location = info.path("end_location");
+      Position start = new Position((float) start_location.path("lat").asDouble(), (float) start_location.path("lng").asDouble());
+      Position end = new Position((float) end_location.path("lat").asDouble(), (float) end_location.path("lng").asDouble());
+
+      String path = info.path("overview_polyline").path("points").asText();
 
       System.out.println(info.path("overview_polyline").path("points").asText());
-      return info.path("overview_polyline").path("points").asText();
+
+      ArrayNode steps = (ArrayNode)info.path("steps");
+
+      for (JsonNode node : steps) {
+          String type = node.path("travel_mode").asText();
+
+          if (type.equals("WALKING")) {
+            continue;
+          }
+
+          // TODO: check for subways
+          // if (type.equals("TRANSIT") && ) {
+
+          // }
+      }
+
+      return new Route(path, start, end);
     } catch (Exception e) {
       e.printStackTrace();
-      return null;
+      return new Route("", new Position(0f, 0f), new Position(0f, 0f));
     }
   }
 }
